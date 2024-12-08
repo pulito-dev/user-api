8413933639154abc18e895b422375ed7349ed253
isakal <sakalivan4@gmail.com>
2024-12-08 16:30:52 +0100

    Add appropriate headers to /auth

--- routes/auth.py
+++ routes/auth.py
@@ -4,15 +4,17 @@ from ..models import *
 from .deps import get_db
 from ..rabbit.client import mq_cl
 from sqlmodel import Session, select
-from fastapi import APIRouter, Depends, HTTPException, Request
+from fastapi import APIRouter, Depends, HTTPException, Request, Response
 
 auth_router = APIRouter()
 
 @auth_router.get("/")
-async def get_forward_auth_req(request: Request) -> Any:
+async def get_forward_auth_req(request: Request, response: Response) -> Any:
     for k,v in request.headers.items():
         if "listing" in k or "listing" in v:
             print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
         print(f"{k}: {v}")
     print(request.path_params)
+    response.headers["authorization"] = request.headers["authorization"]
+
     return {}
